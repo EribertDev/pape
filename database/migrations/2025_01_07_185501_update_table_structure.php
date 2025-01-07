ef0a6dae18ab2d56ea89c6fd6b63d1fc5bd3acfe
Eribert Ganhoun <eribertganhoun7@gmail.com>
2025-01-07 19:59:12 +0100

    syrram_1

--- database/migrations/2025_01_07_185501_update_table_structure.php
+++ database/migrations/2025_01_07_185501_update_table_structure.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -13,14 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('commandes', function (Blueprint $table) {
-            // DB::statement("ALTER TABLE `commandes` CHANGE `annee_academique` `structure_stage` VARCHAR(255)");
-            DB::statement("ALTER TABLE `commandes` CHANGE `specialite` `type_universite` VARCHAR(255)");
-            DB::statement("ALTER TABLE `commandes` CHANGE `niveau` `structure_stage` VARCHAR(255)");
-            $table->string('commune_stage')->nullable();
-            $table->text('fiche_technique')->nullable();
-           
+            //
             $table->dropColumn(['max_pages', 'annee_academique']);
-           
         });
     }
 
@@ -31,7 +24,6 @@ return new class extends Migration
     {
         Schema::table('commandes', function (Blueprint $table) {
             //
-             
         });
     }
 };
